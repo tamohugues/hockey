f812c7bd79b247d580ade74fa5824b9b3951ff0a
hugues tamo tawac <tamo@huguess-iMac.local>
2020-08-29 19:59:35 -0400

    commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'Ceci n\'affichera pas  de nouvelle ligne', PHP_EOL;
+echo 'Ceci n\'affichera pas  de nouvelle ligne', PHP_EOL;
+echo 'Ceci n\'affichera pas  de nouvelle ligne', PHP_EOL;
+echo 'Ceci n\'affichera pas  de nouvelle ligne', PHP_EOL;
